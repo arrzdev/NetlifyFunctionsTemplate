7559a797434b391207d9e8715838e9c3e89c240e
arrz.dev <55147716+arrzdev@users.noreply.github.com>
2021-03-14 00:30:23 +0000

    Update index.js

--- netlify/functions/proxy/index.js
+++ netlify/functions/proxy/index.js
@@ -1,32 +1,14 @@
 const fetch = require("node-fetch")
 
-//We export the function
-exports.handler = async function (event, context) {
-  //Fetch request details from event object
-  const { path, httpMethod, headers, queryStringParameters, body } = event;
-  // return some JSON data with a status of 200
+const body = {
+  authorization: "Njg1MTozMDA4NDg1Mw=="
+}
 
-  const info={
-    authorization: "Njg1MTozMDA4NDg1Mw=="
-  }
-  
-  const response = await fetch("https://servicos.aeof.pt/inovarconsulta/api/loginFU/"), {
+fetch("https://servicos.aeof.pt/inovarconsulta/api/loginFU/", {
   method: "post",
-  body: JSON.stringify(info),
+  body: JSON.stringify(body),
   headers: { "Content-Type": "application/json" }
 })
-
-  .catch(err => console.log(err))
-  const data = await response.json()
-
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "*",
-      "Access-Control-Allow-Headers": "*",
-    },
-    body: JSON.stringify(await data),
-  };
-};
+.then(res => res.json())
+.then(json => console.log(json))
+.catch(err => console.log(err))
